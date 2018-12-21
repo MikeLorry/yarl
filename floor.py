7c927dba3f53f0462054f02d58cadeb72f99cf20
Olivier Gonson <ogonson@touchtunes.com>
2018-12-20 23:09:21 -0500

    add space background generator

--- floor.py
+++ floor.py
@@ -1,5 +1,6 @@
 import pygame
 import json
+import random
 
 class Floor:
     """Common class for floors"""
@@ -9,6 +10,7 @@ class Floor:
             cfg_json = json.loads(f.read())
         self.name = cfg_json["name"]
         self.background = pygame.image.load(cfg_json["background"]).convert_alpha()
+        self.space_bg = self.get_space_background()
         self.map = self.format_map(cfg_json["map"])
         self.origin = cfg_json["origin"].split(":")
         self.origin = [int(x) for x in self.origin]
@@ -28,5 +30,31 @@ class Floor:
     def get_blits(self):
         blits = []
         # Background
-        blits.append((self.background, [0, 0]))
+        #blits.append((self.background, [0, 0]))
+        blits.append((self.space_bg, [0,0]))
         return blits
+
+    def get_space_background(self):
+        screen_size = (400,300)
+        surf_space = pygame.Surface(screen_size)
+        nb_chunk = (self.background.get_size()[0]/50) * (self.background.get_size()[1]/50)
+        chunks = []
+        stars_per_chunk = 10
+        chunk_size = (50,50)
+        for chunk in range(nb_chunk):
+            surf = pygame.Surface(chunk_size)
+            surf.fill((0,0,20))
+            for star in range(stars_per_chunk):
+                pos_x = random.randint(0, chunk_size[0])
+                pos_y = random.randint(0, chunk_size[1])
+                pygame.draw.line(surf, (255,255,255), [pos_x, pos_y], [pos_x,pos_y], 1)
+            chunks.append(surf)
+        pos_x = 0
+        pos_y = 0
+        for chunk in chunks:
+            surf_space.blit(chunk,[pos_x,pos_y])
+            pos_x += chunk_size[0]
+            if pos_x == screen_size[0]:
+                pos_x = 0
+                pos_y += chunk_size[1]
+        return surf_space
